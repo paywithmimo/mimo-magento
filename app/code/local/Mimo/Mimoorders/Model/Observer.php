e1f14ebd64956c76ad259a8799ba5594a0fa3a25
Atul <atulrm@hotmail.com>
2013-05-01 20:03:08 +0530

    Admin Order Module

---
+++ app/code/local/Mimo/Mimoorders/Model/Observer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* MiMo
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@magentocommerce.com so we can send you a copy immediately.
+*
+* @category    Mimo
+* @package     Mimo_Mimoorders
+* @author      MiMo Core Team
+* @copyright   Copyright (c) 2012 MiMo 
+* @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Mimo_Mimoorders_Model_Observer
+{
+    /**
+     * Joins extra tables for adding custom columns to Mage_Adminhtml_Block_Sales_Order_Grid
+     * @param Varien_Object $observer
+     * @return Atwix_Exgrid_Model_Observer
+     */
+    public function salesOrderGridCollectionLoadBefore($observer)
+    {
+        $collection = $observer->getOrderGridCollection();
+        $select = $collection->getSelect();
+        $select->joinLeft(array('payment' => $collection->getTable('sales/order_payment')), 'payment.parent_id=main_table.entity_id', array('payment_method' => 'method'));
+    }
+}
